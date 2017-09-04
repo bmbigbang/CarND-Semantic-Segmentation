343fa092f3b367ce15c73426a570b820d31c6c62
bmbigbang <bm.bigbang@gmail.com>
2017-09-04 15:23:55 +0100

    all tests passed and training proceeds though no information seems to be gained

--- main.py
+++ main.py
@@ -53,7 +53,6 @@ def layers(vgg_layer3_out, vgg_layer4_out, vgg_layer7_out, num_classes):
     :param num_classes: Number of classes to classify
     :return: The Tensor for the last layer of output
     """
-    # TODO: Implement function
 
     l3_num_outputs = int(vgg_layer3_out.shape[-1])
     l4_num_outputs = int(vgg_layer4_out.shape[-1])
@@ -90,7 +89,15 @@ def optimize(nn_last_layer, correct_label, learning_rate, num_classes):
     :param num_classes: Number of classes to classify
     :return: Tuple of (logits, train_op, cross_entropy_loss)
     """
-    # TODO: Implement function
+    logits = tf.reshape(nn_last_layer, (-1, num_classes))
+    correct_label = tf.reshape(correct_label, (-1, num_classes))
+
+    cross_entropy_loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=correct_label))
+
+    optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
+    training_operation = optimizer.minimize(cross_entropy_loss)
+
+    return logits, training_operation, cross_entropy_loss
 tests.test_optimize(optimize)
 
 
@@ -109,8 +116,16 @@ def train_nn(sess, epochs, batch_size, get_batches_fn, train_op, cross_entropy_l
     :param keep_prob: TF Placeholder for dropout keep probability
     :param learning_rate: TF Placeholder for learning rate
     """
-    # TODO: Implement function
-    pass
+
+    # Training computation.
+
+    for epoch in range(epochs):
+        print(epoch)
+        for image, label in get_batches_fn(batch_size):
+            sess.run([train_op], feed_dict={input_image: image, correct_label: label, keep_prob: 0.5,
+                                                      learning_rate: 0.1})
+        print(cross_entropy_loss)
+
 tests.test_train_nn(train_nn)
 
 
@@ -139,10 +154,25 @@ def run():
 
         # TODO: Build NN using load_vgg, layers, and optimize function
 
-        # TODO: Train NN using the train_nn function
+        input_image, keep_prob, layer3_out, layer4_out, layer7_out = load_vgg(sess, vgg_path=vgg_path)
+        layer_output = layers(layer3_out, layer4_out, layer7_out, num_classes)
 
-        # TODO: Save inference data using helper.save_inference_samples
-        #  helper.save_inference_samples(runs_dir, data_dir, sess, image_shape, logits, keep_prob, input_image)
+        epochs = 10
+        batch_size = 24
+
+        learning_rate = tf.placeholder(tf.float32)
+        correct_label = tf.placeholder(tf.float32, [None, None, None, num_classes])
+
+        logits, training_operation, cross_entropy_loss = optimize(layer_output, correct_label, learning_rate,
+                                                                  num_classes)
+
+        sess.run(tf.global_variables_initializer())
+
+        train_nn(sess, epochs, batch_size, get_batches_fn, training_operation, cross_entropy_loss, input_image,
+                 correct_label, keep_prob, learning_rate)
+
+
+        helper.save_inference_samples(runs_dir, data_dir, sess, image_shape, logits, keep_prob, input_image)
 
         # OPTIONAL: Apply the trained model to a video
 
